857559c81450833ababc60949d1436249b36d401
Rogerio Silva Cordeiro <user.nickname>
2023-09-12 22:52:12 -0300

    Versão 2.0

---
+++ Versão_2.0/CadastrarFisicaDTO.java
@@ -0,0 +1,95 @@
+package DTO;
+
+
+public class CadastrarFisicaDTO {
+    private String nome;
+    private String sobrenome;
+    private String endereco;
+    private String numero;  
+    private String telefone;
+    private String dataNascimento;
+    private String cpf;
+    private String senha;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getSobrenome(){
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome){
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public String getDatanascimento(){
+        return dataNascimento;
+    }
+
+    public void setDatanascimento(String dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    public boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
---
+++ Versão_2.0/ClienteFisicaDTO.java
@@ -0,0 +1,49 @@
+package DTO;
+
+
+public class ClienteFisicaDTO {
+    private String id;
+    private String cpf_usuario;
+    private String senha_usuario;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getCpf_usuario(){
+        return cpf_usuario;
+    }
+
+    public void setCpf_usuario(String cpf_usuario){
+        this.cpf_usuario = cpf_usuario;
+    }
+
+    public String senha_usuario(){
+        return senha_usuario;
+    }
+
+    public void setSenha_usuario(String senha_usuario){
+        this.senha_usuario = senha_usuario;
+    }
+
+
+}  
+
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
